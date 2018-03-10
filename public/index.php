72e1f8edc6f27ada7ce2c39e225362f37e6695d4
lhausammann <luz80@gmx.net>
2018-03-10 21:35:36 +0100

    adf

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
-set_include_path(__DIR__ . '/../vendor/vimeo/vimeo-api/src/Vimeo');
+set_include_path(__DIR__ . '/../vendor/vimeo/vimeo-api/src/Vimeo/');
 var_dump(get_include_path());
 var_dump(file_exists(__DIR__.'/../vendor/autoload.php'));
 new Vimeo(123,456);
